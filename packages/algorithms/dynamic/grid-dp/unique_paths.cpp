8ba633538cb977ed7264d476a692157bfaada7cf
Hua Nguyen Khoi <memuopmapdit1234@gmail.com>
2026-03-24 13:05:01 +0000

    feat: adding two file, unique_paths.cpp and unique_paths2.cpp to folder grid-dp

---
+++ packages/algorithms/dynamic/grid-dp/unique_paths.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<vector<int>> matrix;
+
+// Top - down
+int helper(matrix &memo, int i, int j) {
+    // Base case
+    // If i or j is overflow
+    // Return 0
+    if (i < 0 || j < 0) return 0;
+
+    // Else if i == 0, or j == 0
+    // This mean we have a[0][j] or a[i][0]
+    // Return 1 to assign 
+    else if (i == 0 || j == 0) return 1;
+
+    // If calculated 
+    // Get and return it
+    if (memo[i][j] != 0) return memo[i][j];
+    
+    // Else
+    // Assign for memo[i][j]
+    // And return it
+    return memo[i][j] = helper(memo, i - 1, j) + helper(memo, i, j - 1);
+}
+
+// Bottom - up
+int helper(matrix &space, matrix &memo) {
+    int n = space.size(); // Row
+    int m = space[0].size(); // Column
+
+    // Base case
+    // For example
+    /*
+        {1,1,1,1,...}
+        {1,0,0,0,...}
+        {1,0,0,0,...}
+        {1,0,0,0,...}
+    */
+   // For memo
+    for (int i = 0; i < n; i++) memo[i][0] = 1;
+    for (int j = 0; j < m; j++) memo[0][j] = 1;
+
+    // Traversal all elements in memo
+    // And update the number of ways to 
+    // Go to it
+    for (int i = 1; i < n; i++) 
+        for (int j = 1;  j < m; j++) 
+            memo[i][j] = memo[i - 1][j] + memo[i][j - 1];
+    
+    // Return the number of ways to go to
+    // Destination
+    return memo[n - 1][m - 1];
+}
+
+int uniquePaths(matrix &space) {
+    int n = space.size(); // Row
+    int m = space[0].size(); // Column
+
+    matrix memo(n, vector<int>(m, -1));
+    return helper(memo, n - 1, m - 1);
+}
+
+int main() {
+    matrix space(6, vector<int>(6,0));
+    cout << uniquePaths(space) << endl;
+    return 0;
+}
---
+++ packages/algorithms/dynamic/grid-dp/unique_paths2.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<vector<int>> matrix;
+
+void createObstacles(matrix &space, int number) {
+    int n = space.size(); // Row
+    int m = space[0].size(); // Column
+
+    // Create obstacles in space
+    // By random
+    srand(static_cast<unsigned int>(time(0)));
+    
+    for (int i = 0; i < number; i++) 
+        space[1 + rand() % (n - 2)][1 + rand() % (m - 2)] = 1;
+}
+void printMatrix(matrix &space) {
+    int n = space.size();
+    int m = space[0].size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) 
+            cout << space[i][j] << " ";
+        cout << "\n";
+    }
+}
+
+// Top - down
+int helper(matrix &space, matrix &memo, int i, int j) {
+    // Base case
+    // If i and j overflow
+    // Return 0 to advoid to the result
+    if (i < 0 || j < 0) return 0;
+    
+    // If the space at i and j is equal 1
+    // This mean we have been blocked
+    // Return 0 to present do not have path
+    if (space[i][j] == 1) return 0;
+
+    // Else return 1
+    // For each element arr[i][0] or arr[0][j]
+    if (i == 0 || j == 0) return 1;
+    
+    //  If memo existed
+    // Get and return it
+    if (memo[i][j] != -1) return memo[i][j];
+
+    return memo[i][j] = helper(space, memo, i - 1, j) + helper(space, memo, i, j - 1);
+}
+int uniquePaths2(matrix &space) {
+    int n = space.size(); // Row
+    int m = space[0].size(); // Colunm
+
+    // Set up initally
+    matrix memo(n, vector<int>(m, -1));
+
+    return helper(space, memo, n - 1, m - 1);
+}
+
+// Bottom - up
+int uniquePaths2(matrix &space) {
+    int n = space.size(); // Row
+    int m = space[0].size(); // Colunm
+
+    //  Set up  initally
+    matrix memo(n, vector<int>(m, 0));
+
+    // Base case
+    for (int i = 0; i < n; i++)
+        // If the space on border is different from 1
+        // Assign this space in memo by 1
+        // To describe it to have a path
+        if (space[i][0] != 1) memo[i][0] = 1;
+    for (int j = 0; j < m; j++)
+        // Same with row i
+        if (space[0][j] != 1) memo[0][j] = 1;
+        
+    // Traversal all element in space
+    for (int i = 1; i < n; i++) 
+        for (int j = 1; j < m; j++) 
+            // If they are not blocked
+            // Update in memo
+            if (space[i][j] != 1) 
+                memo[i][j] = memo[i - 1][j] + memo[i][j - 1];
+    
+    return memo[n - 1][m - 1];
+}
+
+int main() {
+    matrix space(6, vector<int>(6, 0));
+    createObstacles(space, 12);
+
+    printMatrix(space);
+
+    cout << uniquePaths2(space) << "\n";
+
+    return 0;
+}
